082b1dfc9222a9c32fb0561ccee2e1959ac65399
Antonio Martinez <antonio_halo1@hotmail.com>
2022-10-16 18:42:15 -0500

    Se agregaron comentarios al archivo prime_class.py

--- python/prime_class.py
+++ python/prime_class.py
@@ -1,19 +1,19 @@
-numero = int(input("Ingresa un numero: "))
+numero = int(input("Ingresa un numero: ")) #Con esta linea de codigo le pedimos al usuario que ingrese un número.
 
-def is_prime(numero):
+def is_prime(numero): 
     resultado = True
     contador = 0
     i = 1
 
-    while i < numero:
-        if numero % i == 0:
-            contador = contador + i
+    while i < numero: #Mientras i sea menor que el número ingresado por el usuario el ciclo se repetira.
+        if numero % i == 0: #Con esta condicion el programa ira comprobando si el resultado de una division da como residuo un cero.
+            contador = contador + i #Si la condicion es correcta el contador ira incrementando. 
         i = i + 1
 
-    if contador > 2:
+    if contador > 2: #Si el contador es mayor que 2 entonces el número ingresado no es primo.
         resultado = False
         print(resultado)
-    else:
+    else: #En caso contrario el número ingresado si es primo. 
         print(resultado)
 
 is_prime(numero)
